ea9df2c1639b201f7ae9c37a1068cb46ba05e7fc
Sardius <0xsardius@gmail.com>
2023-09-29 10:34:53 -0400

    Basic tests complete

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -9,6 +9,8 @@ import {BasicNft} from "../src/BasicNft.sol";
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
+    address public USER = makeAddr("user");
+    string public constant OBJECT_PERMANENCE = "https://ipfs.io/ipfs/QmSPf9FVb86gUPgbZx2fER4HJCcx3FCB5DCQ9V9yg6E8J8?filename=No%20object%20Permanence.jpg";
 
     function setUp() public {
         deployer = new DeployBasicNft();
@@ -19,6 +21,13 @@ contract BasicNftTest is Test {
         string memory expectedName = "Doggie";
         string memory actualName = basicNft.name();
         assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(OBJECT_PERMANENCE);
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(keccak256(abi.encodePacked(OBJECT_PERMANENCE)) == keccak256(abi.encodePacked(basicNft.tokenURI(0))));
     }
 
 }
