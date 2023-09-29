8d62b55b90455872a138b073e1b6fde46d059ac4
Sardius <0xsardius@gmail.com>
2023-09-29 13:04:12 -0400

    Interactions update, toml update

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -7,6 +7,7 @@ import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 
 contract MintBasicNft is Script {
+    string public constant OBJECT_PERMANENCE = "https://ipfs.io/ipfs/QmSPf9FVb86gUPgbZx2fER4HJCcx3FCB5DCQ9V9yg6E8J8?filename=No%20object%20Permanence.jpg";
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
         mintNftOnContract(mostRecentlyDeployed);
