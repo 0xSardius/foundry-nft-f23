1f2229fd720b9576785600ed52a0fca92d2c9c57
Sardius <0xsardius@gmail.com>
2023-09-28 20:42:23 -0400

    adding images

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -2,10 +2,22 @@
 
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
     constructor() ERC721("Doggie", "DOG") {
+        s_tokenCounter = 0;
         
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        // return link to IPFS metadata
+        return "";
     }
 }
